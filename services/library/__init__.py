b170589ae3c46a50670af6a54a673843760127e6
Eimy Herrer <fck.police.228@gmail.com>
2026-07-17 18:44:47 +0200

    Bundle 44: export tagged ingestion services

--- services/library/__init__.py
+++ services/library/__init__.py
@@ -1,10 +1,13 @@
 from services.library.identity import ContentTrackIdentityService, TrackIdentityError
 from services.library.importer import LibraryCandidateImporter
+from services.library.ingestion import LibraryTrackIngestionService
 from services.library.metadata import (
     FilenameFallbackMetadataReader,
     MetadataReadError,
+    TinyTagMetadataReader,
     TrackMetadataReader,
 )
+from services.library.persistence import TrackImportPersistenceService
 from services.library.scanner import LibraryScanner
 
 __all__ = [
@@ -12,7 +15,10 @@ __all__ = [
     "FilenameFallbackMetadataReader",
     "LibraryCandidateImporter",
     "LibraryScanner",
+    "LibraryTrackIngestionService",
     "MetadataReadError",
+    "TinyTagMetadataReader",
     "TrackIdentityError",
+    "TrackImportPersistenceService",
     "TrackMetadataReader",
 ]
