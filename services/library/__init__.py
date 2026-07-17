8cc96425b7a80989d4962537b22b0594722918db
Eimy Herrer <fck.police.228@gmail.com>
2026-07-17 18:18:05 +0200

    Bundle 43: export identity and metadata services

--- services/library/__init__.py
+++ services/library/__init__.py
@@ -1,3 +1,18 @@
+from services.library.identity import ContentTrackIdentityService, TrackIdentityError
+from services.library.importer import LibraryCandidateImporter
+from services.library.metadata import (
+    FilenameFallbackMetadataReader,
+    MetadataReadError,
+    TrackMetadataReader,
+)
 from services.library.scanner import LibraryScanner
 
-__all__ = ["LibraryScanner"]
+__all__ = [
+    "ContentTrackIdentityService",
+    "FilenameFallbackMetadataReader",
+    "LibraryCandidateImporter",
+    "LibraryScanner",
+    "MetadataReadError",
+    "TrackIdentityError",
+    "TrackMetadataReader",
+]
